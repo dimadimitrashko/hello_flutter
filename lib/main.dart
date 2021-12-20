46a4d02f461ef9a02c3b7148a37c36aa24669fb4
dimadimitrashko <dimitrashkoo@icloud.com>
2021-12-20 16:32:03 +0200

    nex commit

---
+++ lib/main.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(ScreenWidget());
+
+class ScreenWidget extends StatelessWidget {
+  const ScreenWidget({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('StatelessWidget'),
+        ),
+        body: SimpleWidget(),
+      ),
+    );
+  }
+}
+
+class SimpleWidget extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        child:
+            Center(child: Text('Мой текст', textDirection: TextDirection.ltr)));
+  }
+}
