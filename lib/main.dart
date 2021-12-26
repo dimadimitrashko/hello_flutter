2e0a599c29266cbeb033c7dffde4c4708df80b40
dimadimitrashko <dimitrashkoo@icloud.com>
2021-12-26 17:13:39 +0200

    nex commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,48 +1,18 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(const ScreenWidget());
+void main() => runApp(const Lesson());
 
-class ScreenWidget extends StatelessWidget {
-  const ScreenWidget({Key? key}) : super(key: key);
-  debugShowChecedModeBaner: false;
+class Lesson extends StatelessWidget {
+  const Lesson({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: const Text('StatefullWidget'),
+          title: const Center(child: Text('project')),
         ),
-        body: SimpleWidget(),
-      ),
-    );
-  }
-}
-
-class SimpleWidget extends StatefulWidget {
-  @override
-  _SimpleWidgetState createState() => _SimpleWidgetState();
-}
-
-class _SimpleWidgetState extends State<SimpleWidget> {
-  int count = 0;
-  void _handlButton() {
-    setState((){
-      _count++;
-    });
-  }
-  @override
-  Widget build(BuildContext context) {
-    return Center(
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: <Widget>[
-          Text('$count'),
-          RaisedButton(
-            onPressed: () {_handlButton();},
-            child: Text('Click me!',)
-          )
-        ],
+        body: const Center(child: Text('lah')),
       ),
     );
   }
