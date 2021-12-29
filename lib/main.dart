68e0ac9f85e8b5abaa1e6552fb829258c54e4993
dimadimitrashko <dimitrashkoo@icloud.com>
2021-12-29 09:35:49 +0200

    nex commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,25 +1,10 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(const ScreenWidget());
-
-class ScreenWidget extends StatelessWidget {
-  const ScreenWidget({Key? key}) : super(key: key);
-  debugShowChecedModeBaner: false;
+class Student extends StatelessWidget {
+  const Student({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('StatefullWidget'),
-        ),
-        body: SimpleWidget(),
-      ),
-    );
+    return Container();
   }
-}
-
-class SimpleWidget extends StatefulWidget {
-  @override
-  _SimpleWidgetState createState() => _SimpleWidgetState();
 }
